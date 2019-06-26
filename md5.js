5a259034074ecb05011cae3467caef6cc15907b4
ssdokkaebi <50371502+ssdokkaebi@users.noreply.github.com>
2019-06-26 22:02:48 +0800

    Update md5.js

--- md5.js
+++ md5.js
@@ -1,7 +1,7 @@
 const plaintext = "this is a demo of md5";
 const crypto = require('crypto');
 
-const md5 = crypto.createHash('md5').update(data).digest("hex");
+const md5 = crypto.createHash('md5').update(plaintext).digest("hex");
 console.log('----- Hashing Using MD5 -----');
 console.log('Plaintext: ', plaintext)
 console.log('Hashed: ', md5)
